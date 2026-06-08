96291c695ea08ceb88114bf9c7b3ee6fae12094d
Harsha Koduri <harsha.koduri123@gmail.com>
2026-06-08 07:13:06 +0000

    Initial commit

--- backend/urls.py
+++ backend/urls.py
@@ -2,25 +2,22 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse
+from django.views.generic import TemplateView
 
 from .media_views import serve_media
 
 
-def home(request):
-    return JsonResponse({
-        "message": "Backend is running",
-        "status": "ok"
-    })
-
-
 urlpatterns = [
-    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("training_app.urls")),
 
     re_path(r"^media/(?P<path>.*)$", serve_media),
     re_path(r"^api/media/(?P<path>.*)$", serve_media),
+
+    re_path(
+        r"^(?!api/|admin/|media/|static/|assets/).*$",
+        TemplateView.as_view(template_name="index.html"),
+    ),
 ]
 
 if settings.DEBUG:
